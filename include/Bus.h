0c8ec2d861535527eb756d4838e567fb3d0512bf
onecancat <onecancat@gmail.com>
2026-01-18 01:24:25 +0530

    bus arrays

--- include/Bus.h
+++ include/Bus.h
@@ -9,7 +9,12 @@ public:
   ~Bus();
 
 public: 
-  std::array<uint8_t, 64 * 1024> ram;
+  std::array<uint8_t, 0x2000> VRAM; // 8000
+  std::array<uint8_t, 0x2000> WRAM; // C000
+  std::array<uint8_t, 0x00A0> OAM;  // FE00
+  std::array<uint8_t, 0x007F> HRAM; // FF80
+  std::array<uint8_t, 0x80> IO;
+  uint8_t IE;
 
 public:
   void write(uint16_t addr, uint8_t data);
---
+++ include/cartridge.h
@@ -0,0 +1,7 @@
+
+
+class Cartridge {
+public:
+	std::vector<uint8_t> ROM;
+	std::vector<uint8_t> RAM;
+};

b3853f18fed1d1551e6cb819a1750509465e3823
onecancat <onecancat@gmail.com>
2026-01-20 23:20:16 +0530

    added bus write addr decode logic, removed constructor initializing of arrays

--- include/Bus.h
+++ include/Bus.h
@@ -13,11 +13,11 @@ public:
 private: 
 	Cartridge* cart = nullptr;
   std::array<uint8_t, 0x2000> VRAM; // 8000
-  std::array<uint8_t, 0x2000> WRAM; // C000
+  std::array<uint8_t, 0x2000> WRAM{}; // C000
   std::array<uint8_t, 0x00A0> OAM;  // FE00
-  std::array<uint8_t, 0x007F> HRAM; // FF80
-  std::array<uint8_t, 0x80> IO;
-  uint8_t IE;
+  std::array<uint8_t, 0x007F> HRAM{}; // FF80
+  std::array<uint8_t, 0x80> IO{};
+  uint8_t IE{};
 
 public:
   void write(uint16_t addr, uint8_t data);
--- src/Bus.cpp
+++ src/Bus.cpp
@@ -1,29 +1,38 @@
 #include "Bus.h"
 #include "cartridge.h"
+#include <cassert>
 
-Bus::Bus() {
-  for (auto &i : WRAM)
-      i = 0x00;
-  for (auto& i : HRAM)
-      i = 0x00;
-  for (auto& i : IO)
-      i = 0x00;
-  IE = 0x00;
-}
+Bus::Bus() {}
 
 Bus::~Bus() {}
 
 void Bus::write(uint16_t addr, uint8_t data) {
-  if (addr >= 0x0000 && addr <= 0xFFFF) {
-    ram[addr] = data;
-  }
+    if (addr >= 0x0000 && addr <= 0x7FFF) {
+        assert(cart != nullptr);
+        cart->write(addr, data);
+    } else if (addr >= 0x8000 && addr <= 0x9FFF) VRAM[addr - 0x8000] = data;
+    else if (addr >= 0xA000 && addr <= 0xBFFF) {
+        assert(cart != nullptr);
+        cart->write(addr, data);
+    } else if (addr >= 0xC000 && addr <= 0xDFFF) WRAM[addr - 0xC000] = data;
+    else if (addr >= 0xE000 && addr <= 0xFDFF) WRAM[addr - 0xE000] = data;
+    else if (addr >= 0xFE00 && addr <= 0xFE9F) OAM[addr - 0xFE00] = data;
+    else if (addr >= 0xFEA0 && addr <= 0xFEFF); // not usable
+    else if (addr >= 0xFF00 && addr <= 0xFF7F) IO[addr - 0xFF00] = data;
+    else if (addr >= 0xFF80 && addr <= 0xFFFE) HRAM[addr - 0xFF80] = data;
+    else if (addr == 0xFFFF) IE = data;
+    else;
 }
 
 uint8_t Bus::read(uint16_t addr) {
-    if (addr >= 0x0000 && addr <= 0x7FFF) return cart->read(addr); // rom
-    else if (addr >= 0x8000 && addr <= 0x9FFF) return VRAM[addr - 0x8000]; // vram
-    else if (addr >= 0xA000 && addr <= 0xBFFF) return cart->read(addr); // ram
-    else if (addr >= 0xC000 && addr <= 0xDFFF) return WRAM[addr - 0xC000]; // wram
+    if (addr >= 0x0000 && addr <= 0x7FFF) {
+        assert(cart != nullptr);
+        return cart->read(addr); // rom
+    } else if (addr >= 0x8000 && addr <= 0x9FFF) return VRAM[addr - 0x8000]; // vram
+    else if (addr >= 0xA000 && addr <= 0xBFFF) {
+        assert(cart != nullptr);
+        return cart->read(addr); // ram
+    } else if (addr >= 0xC000 && addr <= 0xDFFF) return WRAM[addr - 0xC000]; // wram
     else if (addr >= 0xE000 && addr <= 0xFDFF) return WRAM[addr - 0xE000]; // echo ram
     else if (addr >= 0xFE00 && addr <= 0xFE9F) return OAM[addr - 0xFE00]; // oam
     else if (addr >= 0xFEA0 && addr <= 0xFEFF) return 0xFF; // not usable

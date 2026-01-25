34df5888b93b59c4c130216a085f2d4dc3ad75eb
onecancat <onecancat@gmail.com>
2026-01-25 10:12:05 +0530

    system testing with test roms

--- src/main.cpp
+++ src/main.cpp
@@ -1,78 +1,57 @@
 #include "Emu.h"
 #include <iostream>
-#include <fstream>
-#include <filesystem>
-
-void loadTest(Emu& gb, const char* path, uint16_t base);
 
 int main() {
     Emu gb;
 
+
+    gb.cpu.regs.pc = 0x0000;
+    gb.cpu.regs.sp = 0xFFFE;
+    gb.cart.loadGame("../../../src/test.gb");
+    gb.bus.attachCart(&gb.cart);
     
-  gb.bus.write(0x0000, 0xC9);
-  loadTest(gb, "../../../src/test.bin", 0xC000);
-
-  gb.cpu.regs.pc = 0xC000;
-  gb.cpu.regs.sp = 0xCFFF;
-
-  std::cout << "PC before clock = "
-      << std::hex << gb.cpu.regs.pc << "\n";
+    std::cout << "PC before clock = "
+        << std::hex << gb.cpu.regs.pc << "\n";
 
 
-  while (!gb.cpu.isHalted()) {
-      gb.cpu.step();
-  } 
+    while (!gb.cpu.isHalted()) {
+        uint16_t pc = gb.cpu.regs.pc;
+        uint8_t op = gb.bus.read(pc);
 
-  auto& r = gb.cpu.regs;
+        std::cout << "PC=" << std::hex << pc
+            << " OP=" << (int)op << "\n";
 
-  std::cout << std::hex;
-  std::cout << "==== CPU STATE ====\n";
-  std::cout << "is halted or not" << (int)gb.cpu.isHalted() << std::endl;
-  std::cout << "PC=" << r.pc << " SP=" << r.sp << "\n";
-
-  std::cout << "A=" << (int)r.a << " F=" << (int)r.f
-      << "  ["
-      << ((r.f & 0x80) ? 'Z' : '-')
-      << ((r.f & 0x40) ? 'N' : '-')
-      << ((r.f & 0x20) ? 'H' : '-')
-      << ((r.f & 0x10) ? 'C' : '-')
-      << "]\n";
-
-  std::cout << "B=" << (int)r.b << " C=" << (int)r.c << "\n";
-  std::cout << "D=" << (int)r.d << " E=" << (int)r.e << "\n";
-  std::cout << "H=" << (int)r.h << " L=" << (int)r.l << "\n";
-  std::cout << "BC=" << (int)r.bc << " DE=" << (int)r.de << "\n";
-  std::cout << "HL=" << (int)r.hl << " SP=" << (int)r.sp << "\n";
-
-  std::cout << "\n==== MEMORY DUMP ====\n";
-
-  uint8_t mIE = gb.bus.read(0xFFFF);
-  uint8_t mIF = gb.bus.read(0xFF0F);
-
-  std::cout << std::hex;
-  std::cout << "IE = " << (int)mIE << "\n";
-  std::cout << "IF = " << (int)mIF << "\n";
-  std::cout << "c100" << (int)gb.bus.read(0xc100) << std::endl;
-  std::cout << "c101" << (int)gb.bus.read(0xc101) << std::endl;
-  std::cout << "c102" << (int)gb.bus.read(0xc102) << std::endl;
-  std::cout << "c103" << (int)gb.bus.read(0xc103) << std::endl;
-
-
-  return 0;
-}
-
-
-void loadTest(Emu& gb, const char* path, uint16_t base) {
-    std::ifstream file(path, std::ios::binary);
-    uint8_t byte;
-    uint16_t addr = base;
-
-    if (!file.is_open()) {
-        std::cerr << "FAILED to open file: " << path << "\n";
-        return;
+        gb.cpu.step();
     }
 
-    while (file.read(reinterpret_cast<char*>(&byte), 1)) {
-        gb.bus.write(addr++, byte);
-    }
+    auto& r = gb.cpu.regs;
+
+    std::cout << std::hex;
+    std::cout << " CPU STATE\n";
+    std::cout << "is halted or not" << (int)gb.cpu.isHalted() << std::endl;
+    std::cout << "PC=" << r.pc << " SP=" << r.sp << "\n";
+
+    std::cout << "A=" << (int)r.a << " F=" << (int)r.f
+        << "  ["
+        << ((r.f & 0x80) ? 'Z' : '-')
+        << ((r.f & 0x40) ? 'N' : '-')
+        << ((r.f & 0x20) ? 'H' : '-')
+        << ((r.f & 0x10) ? 'C' : '-')
+        << "]\n";
+
+    std::cout << "B=" << (int)r.b << " C=" << (int)r.c << "\n";
+    std::cout << "D=" << (int)r.d << " E=" << (int)r.e << "\n";
+    std::cout << "H=" << (int)r.h << " L=" << (int)r.l << "\n";
+    std::cout << "BC=" << (int)r.bc << " DE=" << (int)r.de << "\n";
+    std::cout << "HL=" << (int)r.hl << " SP=" << (int)r.sp << "\n";
+
+    std::cout << "\n MEMORY DUMP \n";
+
+    uint8_t mIE = gb.bus.read(0xFFFF);
+    uint8_t mIF = gb.bus.read(0xFF0F);
+    std::cout << std::hex;
+    std::cout << "IE = " << (int)mIE << "\n";
+    std::cout << "IF = " << (int)mIF << "\n";
+
+    return 0;   
 }

ce3a7925227b0f9da69b417a4a8a8da3bfa0a2d0
onecancat <onecancat@gmail.com>
2026-02-25 00:05:41 +0530

    vram and oam blocking

--- src/Bus.cpp
+++ src/Bus.cpp
@@ -11,13 +11,28 @@ void Bus::write(uint16_t addr, uint8_t data) {
     if (addr >= 0x0000 && addr <= 0x7FFF) {
         assert(cart != nullptr);
         cart->write(addr, data);
-    } else if (addr >= 0x8000 && addr <= 0x9FFF) VRAM[addr - 0x8000] = data;
+    }
+    else if (addr >= 0x8000 && addr <= 0x9FFF) { // vram
+        uint8_t stat = io->readSTAT();
+        uint8_t lcdc = io->readLCDC();
+        uint8_t mode = stat & 0x03;
+        uint8_t ENlcd = lcdc & 0x80;
+        if (ENlcd && mode == 3) return;
+        VRAM[addr - 0x8000] = data;
+    }
     else if (addr >= 0xA000 && addr <= 0xBFFF) {
         assert(cart != nullptr);
         cart->write(addr, data);
     } else if (addr >= 0xC000 && addr <= 0xDFFF) WRAM[addr - 0xC000] = data;
     else if (addr >= 0xE000 && addr <= 0xFDFF) WRAM[addr - 0xE000] = data;
-    else if (addr >= 0xFE00 && addr <= 0xFE9F) OAM[addr - 0xFE00] = data;
+    else if (addr >= 0xFE00 && addr <= 0xFE9F) { // oam
+        uint8_t stat = io->readSTAT();
+        uint8_t lcdc = io->readLCDC();
+        uint8_t mode = stat & 0x03;
+        uint8_t ENlcd = lcdc & 0x80;
+        if (ENlcd && (mode == 2 || mode == 3)) return;
+        OAM[addr - 0xFE00] = data;
+    }
     else if (addr >= 0xFEA0 && addr <= 0xFEFF); // not usable
     else if (addr >= 0xFF00 && addr <= 0xFF7F) io->write(addr, data);
     else if (addr >= 0xFF80 && addr <= 0xFFFE) HRAM[addr - 0xFF80] = data;
@@ -29,13 +44,29 @@ uint8_t Bus::read(uint16_t addr) {
     if (addr >= 0x0000 && addr <= 0x7FFF) {
         assert(cart != nullptr);
         return cart->read(addr); // rom
-    } else if (addr >= 0x8000 && addr <= 0x9FFF) return VRAM[addr - 0x8000]; // vram
+    }
+    else if (addr >= 0x8000 && addr <= 0x9FFF) {  //vram
+        uint8_t stat = io->readSTAT();
+        uint8_t lcdc = io->readLCDC();
+        uint8_t mode = stat & 0x03;
+        uint8_t ENlcd = lcdc & 0x80;
+        if (ENlcd && mode == 3) return 0xFF;
+        return VRAM[addr - 0x8000];
+    }
     else if (addr >= 0xA000 && addr <= 0xBFFF) {
         assert(cart != nullptr);
         return cart->read(addr); // ram
-    } else if (addr >= 0xC000 && addr <= 0xDFFF) return WRAM[addr - 0xC000]; // wram
+    } 
+    else if (addr >= 0xC000 && addr <= 0xDFFF) return WRAM[addr - 0xC000]; // wram
     else if (addr >= 0xE000 && addr <= 0xFDFF) return WRAM[addr - 0xE000]; // echo ram
-    else if (addr >= 0xFE00 && addr <= 0xFE9F) return OAM[addr - 0xFE00]; // oam
+    else if (addr >= 0xFE00 && addr <= 0xFE9F) { //oam
+        uint8_t stat = io->readSTAT();
+        uint8_t lcdc = io->readLCDC();
+        uint8_t mode = stat & 0x03;
+        uint8_t ENlcd = lcdc & 0x80;
+        if (ENlcd && (mode == 2 || mode == 3)) return 0xFF;
+        return OAM[addr - 0xFE00];
+    }
     else if (addr >= 0xFEA0 && addr <= 0xFEFF) return 0xFF; // not usable
     else if (addr >= 0xFF00 && addr <= 0xFF7F) return io->read(addr); // io
     else if (addr >= 0xFF80 && addr <= 0xFFFE) return HRAM[addr - 0xFF80]; // hram

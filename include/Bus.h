f16fc87c8dd359cab40fc0263b65177b3bad22a4
onecancat <onecancat@gmail.com>
2026-02-25 00:52:47 +0530

    oam dma transfer

--- include/Bus.h
+++ include/Bus.h
@@ -19,10 +19,18 @@ private:
   std::array<uint8_t, 0x00A0> OAM;  // FE00
   std::array<uint8_t, 0x007F> HRAM{}; // FF80
   uint8_t IE{};
+  bool dmaActive = false;
+  uint16_t dmaSource = 0;
+  uint16_t dmaTicks = 0;
+  uint8_t dmaIndex = 0;
+  void startDMA(uint8_t val);
+  void tickDMA();
+  bool isDMAActive();
 
 public:
   void write(uint16_t addr, uint8_t data);
   uint8_t read(uint16_t addr);
+  uint8_t dmaRead(uint16_t addr);
   void attachCart(Cartridge *c);
   void attachIO(IO* i);
 };
--- src/Bus.cpp
+++ src/Bus.cpp
@@ -81,3 +81,45 @@ void Bus::attachCart(Cartridge* c) {
 void Bus::attachIO(IO* i) {
     io = i;
 }
+
+void Bus::startDMA(uint8_t val) {
+    dmaSource = val << 8;
+    dmaTicks = 0;
+    dmaIndex = 0;
+    dmaActive = true;
+}
+
+bool Bus::isDMAActive() {
+    return dmaActive;
+}
+
+uint8_t Bus::dmaRead(uint16_t addr) {
+    if (addr >= 0x0000 && addr <= 0x7FFF) {
+        assert(cart != nullptr);
+        return cart->read(addr); // rom
+    }
+    else if (addr >= 0x8000 && addr <= 0x9FFF) return VRAM[addr - 0x8000]; // vram
+    else if (addr >= 0xA000 && addr <= 0xBFFF) {
+        assert(cart != nullptr);
+        return cart->read(addr); // ram
+    }
+    else if (addr >= 0xC000 && addr <= 0xDFFF) return WRAM[addr - 0xC000]; // wram
+    else if (addr >= 0xE000 && addr <= 0xFDFF) return WRAM[addr - 0xE000]; // echo ram
+    else if (addr >= 0xFE00 && addr <= 0xFE9F) return OAM[addr - 0xFE00]; // oam
+    else if (addr >= 0xFEA0 && addr <= 0xFEFF) return 0xFF; // not usable
+    else if (addr >= 0xFF00 && addr <= 0xFF7F) return io->read(addr); // io
+    else if (addr >= 0xFF80 && addr <= 0xFFFE) return HRAM[addr - 0xFF80]; // hram
+    else if (addr == 0xFFFF) return IE;
+    else return 0xFF;
+}
+
+void Bus::tickDMA() {
+    dmaTicks++;
+    if (dmaTicks % 4 == 0 && dmaIndex < 160) {
+        OAM[dmaIndex] = dmaRead(dmaSource + dmaIndex);
+        dmaIndex++;
+    }
+    if (dmaTicks == 640) {
+        dmaActive = false;
+    }
+}
